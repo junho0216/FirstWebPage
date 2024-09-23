dd47631b25dde29cf7a685fb6d762d545bcf9332
junho0216 <24195@dongdaemun.ms.kr>
2024-09-23 15:32:51 +0900

    Create 02-text.py

---
+++ pages/02-text.py
@@ -0,0 +1,14 @@
+import streamlit as at
+ 
+# 타이틀 적용 예시
+st.title('타이틀은 페이지 제목')
+
+# 특수 이모티콘 삼입 예시
+#  emoji: htts://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
+st.title('스마일 :sparkles:')
+
+# Header 적용
+st.header('헤더는 섹션의 제목 :sparkles:')
+
+#Subheader 적용
+st.subheader('subheader는  섹션의 부제목')
