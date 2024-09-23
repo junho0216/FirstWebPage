d90582cd3aff200233795510e05c09b836d1d912
junho0216 <24195@dongdaemun.ms.kr>
2024-09-23 15:43:36 +0900

    Update 02-text.py

--- pages/02-text.py
+++ pages/02-text.py
@@ -12,3 +12,18 @@ st.header('헤더는 섹션의 제목 :sparkles:')
 
 #Subheader 적용
 st.subheader('subheader는  섹션의 부제목')
+
+
+
+#캡션 적용
+st.caption('캡션은 짧은 설명을 추가하는 것! '')
+
+#코드 표시
+sample_code='''
+def function():
+   print('hello,  world')
+...
+st.code(sample_code, language="python")
+
+# 일반 테스트
+st.text('일반적인 텍스트를 입력해 보았습니다.')
