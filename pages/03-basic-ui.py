a266fd171d87a12705a941420a99a876824ba5ad
junho0216 <24195@dongdaemun.ms.kr>
2024-09-30 15:23:31 +0900

    Update 03-basic-ui.py

--- pages/03-basic-ui.py
+++ pages/03-basic-ui.py
@@ -1,9 +1,9 @@
 import streamlit as at
-import pandas as p
+import pandas as pd
 from datetime import datetime as dt
 import datetime
 
 #버튼 클릭
 button = st.button('버튼을 눌러보세요')
 if button:
-     st.write(':blue[버튼]이 눌렸습니다 :sparkles
+     st.write(':blue[버튼]이 눌렸습니다 :sparkles:')
